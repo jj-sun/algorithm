b4e08a474ceaed4bd9523a70593a70322b50d478
sunjie <sunjie.one@outlook.com>
2019-05-14 16:42:14 +0800

    新题

---
+++ leetcode/src/main/java/com/algorithm/code_1_100/code_18/Solution.java
@@ -0,0 +1,70 @@
+package com.algorithm.code_1_100.code_18;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，
+ * 使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+ * <p>
+ * 注意：
+ * <p>
+ * 答案中不可以包含重复的四元组。
+ * <p>
+ * 示例：
+ * <p>
+ * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+ * <p>
+ * 满足要求的四元组集合为：
+ * [
+ * [-1,  0, 0, 1],
+ * [-2, -1, 1, 2],
+ * [-2,  0, 0, 2]
+ * ]
+ */
+
+public class Solution {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        if (nums.length < 4) {
+            return res;
+        }
+        for (int i = 0; i < nums.length - 3; i++) {
+            int new_target = target - nums[i];
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int l = j + 1, r = nums.length - 1;
+
+                while (l < r) {
+
+                    if (nums[j] + nums[l] + nums[r] == new_target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) {
+                            l++;
+                        }
+                        while (l < r && nums[r] == nums[r - 1]) {
+                            r--;
+                        }
+                        l++;
+                        r--;
+                    } else if (nums[j] + nums[l] + nums[r] < new_target) {
+                        l++;
+                    } else {
+                        r--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+}
--- leetcode/src/main/java/com/algorithm/code_1_100/code_22/Solution.java
+++ leetcode/src/main/java/com/algorithm/code_1_100/code_22/Solution.java
@@ -1,22 +1,60 @@
 package com.algorithm.code_1_100.code_22;
 
-public class Solution {
-    public int removeElement(int[] nums, int val) {
-       int i=0;
-       for(int j=0; j<nums.length; j++) {
-           if(nums[j] != val) {
-               nums[i] = nums[j];
-               i++;
-           }
-       }
-       return i;
-    }
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args) {
-        int nums[] = {0,1,2,2,3,0,4,2};
-        System.out.println(new Solution().removeElement(nums,2));
-        for(int i=0; i<nums.length; i++) {
-            System.out.println(nums[i]);
+/**
+ * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+ * <p>
+ * 例如，给出 n = 3，生成结果为：
+ * <p>
+ * [
+ * "((()))",
+ * "(()())",
+ * "(())()",
+ * "()(())",
+ * "()()()"
+ * ]
+ */
+
+public class Solution {
+
+    /**
+     * 1.左括号用left表示剩余数量，右括号用right表示剩余数量。
+     * 2.递归必须要有结束递归的条件，我们这个题目结束的条件就可以用剩余数量来决定
+     * 3.结束递归有两种，
+     *     A.一种是错了，如何结束？比如left > right,证明右括号用的多了，右括号用多了肯定会没办法最终组成。
+     *     B.另一种是正确的，如何结束？比如left == 0 && right == 0 ,都用完了，既然满足上边的条件，那么肯定是正常结束，push_back即可。
+     * 4.递归调用也分着调用。
+     *     left左括号先递归，right右括号的后递归。
+     * @param n
+     * @return
+     */
+
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        dfs("",n,n,res);
+        return res;
+    }
+    private void dfs(String str,int left,int right,List<String> res) {
+        if(left > right) {
+            return;
+        }
+        if(left == 0 && right == 0) {
+            res.add(str);
+        } else {
+            if(left > 0) {
+                dfs(str + "(",left-1,right,res);
+            }
+            if(right > 0) {
+                dfs(str + ")",left,right-1,res);
+            }
         }
     }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().generateParenthesis(3));
+    }
+
 }
